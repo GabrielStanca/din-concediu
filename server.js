250d0628fd76048164fc5de829bec1597a5133e5
SORON <Arclightsorin@gmail.com>
2023-03-29 22:58:51 +0300

    On branch api
      Created a user model and a path for creating new users to test the connection to the Atlas DB. It Works.
      Installed bcryptjs to hash passwords.
    
      Changes to be committed:
    	new file:   models/User.js
    	new file:   routes/auth.js
    
    	modified:   server.js
    
    	modified:   .env
    	modified:   package-lock.json
    	modified:   package.json

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+// Import Routes
+const authRoute = require("./routes/auth")
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get("/", (req, res) =>{
+app.get("/api", (req, res) => {
     res.send("It works");
 });
 
-app.post("/name", (req,res) => {
-    if(req.body.name) {
-        return res.json({name: req.body.name});
+app.post("/name", (req, res) => {
+    if (req.body.name) {
+        return res.json({ name: req.body.name });
     } else {
-        return res.status(400).json({error:"No name provided"});
+        return res.status(400).json({ error: "No name provided" });
     }
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`The Server is Running on PORT: ${process.env.PORT}`);
-})
+app.use("/api/auth", authRoute);
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to DB");
+
+        app.listen(process.env.PORT, () => {
+            console.log(`The Server is Running on PORT: ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
