8cc766eeeb769813f0982e47e7aab53c7f058fc9
GabrielStanca <gabriel.s@menskin.co>
2023-04-04 15:54:28 +0300

    make conversation functionality

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,62 +1,38 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "../style/chat.css"
 import PeopleCardList from "./reusableComponents/peopleCardList";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
-import {useGlobalContext} from "../context/GlobalContext";
 import {getAllUsers} from "../services/getAllUsers";
+import {useGlobalContext} from "../context/GlobalContext";
+import io from "socket.io-client"
+import ChatConversation from "./chat-conversation";
+
+const socket = io.connect("http://localhost:5000")
 
 
 const Chat= ()=> {
+    const [userList, setUserList] = useState([]);
 
-    getAllUsers().then(users => console.log(users))
-
-
-    const userList = [{
-        firstName: "Marinica",
-        lastName: "Gigel",
-        email: "ionelgigel23@gmail.com",
-        phone: "0753548789"
-    },
-        {
-            firstName: "Ionel",
-            lastName: "Fanel",
-            email: "ionelgigel23@gmail.com",
-            phone: "0753548789"
-        },
-        {
-            firstName: "Ionel",
-            lastName: "Gigel",
-            email: "ionelgigel23@gmail.com",
-            phone: "0753548789"
-        },
-        {
-            firstName: "Ionel",
-            lastName: "Gigel",
-            email: "ionelgigel23@gmail.com",
-            phone: "0753548789"
-        },
-        {
-            firstName: "Ionel",
-            lastName: "Gigel",
-            email: "ionelgigel23@gmail.com",
-            phone: "0753548789"
-        },
-        {
-            firstName: "Ionel",
-            lastName: "Gigel",
-            email: "ionelgigel23@gmail.com",
-            phone: "0753548789"
-        },{
-            firstName: "Ionel",
-            lastName: "Gigel",
-            email: "ionelgigel23@gmail.com",
-            phone: "0753548789"
-        },]
+    useEffect(() => {
+        getAllUsers().then(users => setUserList(users));
+    }, []);
 
     const [filter, setFilter] = useState("");
     const {user} = useGlobalContext()
-    console.log(user,"aaaa")
+    let [email,domain] = ""
+    if(user) {
+        [email,domain] = user.email.split("@")
+    }
+
+    const [room,setRoom] = useState("")
+
+    const joinRoom = (room) => {
+        if(room !== ""){
+            socket.emit("join_room",room)
+        }
+    }
+
     return (
         <div className="chat_container">
             <div className="chat_list">
@@ -66,22 +42,35 @@ const Chat= ()=> {
                         setFilter( e.target.value)
                     }}/>
                 </div>
-
                 <div className="list_constainer" style={{overflowY:"auto"}}>
                     <div>
-                        {userList.filter((user)=> {
-                            const fullName = user.firstName +" " +user.lastName;
-                            return fullName.toLowerCase().includes(filter.toLowerCase())
-                        }).map((user,key)=> {
-                            return(<PeopleCardList key={key} user={user}/>)
-                        })}
+                        {userList !== [] ? (userList.filter((filteredUser)=> {
+                            const fullName = filteredUser.firstName +" " +filteredUser.lastName;
+                            if(filteredUser.email.includes(domain) && user._id !== filteredUser._id)
+                                return fullName.toLowerCase().includes(filter.toLowerCase())
+                        }).map((filteredUser,key)=> {
+                            return(
+                                <span
+                                    style={{marginBottom:"20px"}}
+                                    onClick={()=> {
+                                    setRoom(`${filteredUser._id}_`)
+                                    joinRoom(`${filteredUser._id}_`)
+                                    }}
+                                >
+                                    <button onClick={()=>joinRoom()}>Join a Room</button>
+                                    <PeopleCardList key={key} user={filteredUser}/>
+                                </span>
+                            )
+                        })) : (<p>Loading...</p>)}
                     </div>
 
                 </div>
 
             </div>
             <div className="chat_conversation">
-                <p>Actual Chat</p>
+                {user ? (
+                    <ChatConversation socket={socket} username={user.firstName} room={room}/>
+                ): (<p>Loading...</p>)}
             </div>
         </div>
     );
